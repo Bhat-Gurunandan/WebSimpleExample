d9a28dea55b3d3cc3b6035d7e6c2796bfd24abf8
Gurunandan Bhat <gbhat@pobox.com>
2016-09-02 00:39:29 +0200

    Added repo sync and build methods

--- simple/simple.pl
+++ simple/simple.pl
@@ -2,6 +2,7 @@
 
 use Web::Simple qw/MyApplication/;
 
+
 {
     package MyApplication;
 
@@ -12,18 +13,80 @@ use Web::Simple qw/MyApplication/;
     use Digest::HMAC_SHA1 qw/hmac_sha1_hex/;
     use JSON;
 
+    my $config = {
+        git             => '/usr/bin/git',
+        secret          => '********',
+        git_work_tree   => '/home/nandan/repos/AlmostIsland',
+        committer_email => 'gbhat@pobox.com',
+        committer_name  => 'Gurunandan Bhat',
+        site_builder    => '/home/nandan/repos/AICode/bin/aiweb.pl test',
+        email_list      => [
+            'gbhat@pobox.com',
+            'rhymebawd@gmail.com',
+        ],
+    };
+
     sub dispatch_request {
 
-        'GET + /' => sub {
+        'POST + /sync' => sub {
+
             my ($self, $env) = @_;
-            debugf($self);
-            infof($env);
+            my $repo    = $self->repo($env);
+
             return [
                 200,
                 ['Content-type' => 'text/plain'],
-                [ np($self) . "\n" . np($env) ]
+                [ $repo->build ],
             ];
-        };
+        }
+    }
+
+    sub repo {
+
+        my ($self, $env) = @_;
+        my $req = Plack::Request->new($env);
+
+        $payload = decode_json( $req->raw_body );
+        my $digest  = $req->headers->header('X-Hub-Signature');
+        my $check   = 'sha1=' . hmac_sha1_hex($payload, 'StriverConniver');
+
+        return Git::Repository->new(
+            work_tree => '/home/nandan/repos/AlmostIsland', {
+                git => '/usr/local/bin/git',
+                env => {
+                    GIT_COMMITTER_EMAIL => 'gbhat@pobox.com',
+                    GIT_COMMITTER_NAME  => 'Gurunandan Bhat',
+                },
+            }
+        );
+    }
+
+    sub build {
+
+        my $self = shift;
+
+        my $head_commit = $self->repo->run('rev-parse' => 'HEAD');
+
+        my @log;
+        if ( $head_commit ne $payload->{head_commit}{id} ) {
+
+            @log = $self->run(reset => '--hard', 'mycopy/master');
+            push @log, ($self->run(pull => 'mycopy',  'master'));
+
+            my @action = `/home/nandan/repos/AICode/bin/aiweb.pl test`;
+            push @log, @action;
+
+            push @log, ($self->run(add => '.'));
+            push @log, ($self->run(commit => '-m', sprintf('Automated Build %s', scalar localtime)));
+            push @log, ($self->run(push => 'mycopy', 'master'));
+            push @log, ($self->run(push => 'origin', 'master'));
+        }
+
+        return Dumper(\@log);
+    }
+
+    sub send_email {
+
     }
 
     around to_psgi_app => sub {
